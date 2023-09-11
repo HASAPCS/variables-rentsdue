b20d5fd1441af8196ca12662cac1a00bc1033a93
rentsdue <hoganwong88@gmail.com>
2023-09-11 12:30:39 +0800

    fix errors

--- src/main/java/com/example/VariablesAssignment.java
+++ src/main/java/com/example/VariablesAssignment.java
@@ -3,28 +3,28 @@ package com.example;
 public class VariablesAssignment {
 
     // TODO: Declare and initialize a byte variable with any value between 1 and 127
-    byte byteVariable=69
+    byte byteVariable=69;
 
     // TODO: Declare and initialize a short variable with any value between 1 and 32767
-    short shortVariable=420
+    short shortVariable=420;
 
     // TODO: Declare and initialize an int variable with any positive value
-    int intVariable=22
+    int intVariable=22;
 
     // TODO: Declare and initialize a long variable with any positive value
-    long longVariable=33
+    long longVariable=333L;
 
     // TODO: Declare and initialize a float variable with any positive value
-    float floatVariable=22.45
+    float floatVariable=22.45f;
 
     // TODO: Declare and initialize a double variable with any positive value
-    double doubleVariable=22.55
+    double doubleVariable=22.55d;
 
     // TODO: Declare and initialize a char variable with the initial of your first name
-    char charVariable='H'
+    char charVariable='H';
 
     // TODO: Declare and initialize a boolean variable with the value true or false
-    boolean booleanVariable=True
+    boolean booleanVariable=true;
 
     // TODO: Create a method that accepts two integers as parameters and returns their sum
     public int sum(int a, int b) {
@@ -36,27 +36,17 @@ public class VariablesAssignment {
         return a*b; // Change this line to return the actual product
     }
 
-    // TODO: Create a method that accepts a string parameter and returns it in reverse
-    public class ReverseString {
+    // Method to reverse a string
+    public static String reverseString(String input) {
+        StringBuilder reversed = new StringBuilder();
 
-        // Method to reverse a string
-        public static String reverseString(String input) {
-            StringBuilder reversed = new StringBuilder();
-    
-            // Loop through the input string in reverse order and append each character to the 'reversed' StringBuilder
-            for (int i = input.length() - 1; i >= 0; i--) {
-                reversed.append(input.charAt(i));
-            }
-    
-            // Convert the StringBuilder to a String and return it
-            return reversed.toString();
+        // Loop through the input string in reverse order and append each character to the 'reversed' StringBuilder
+        for (int i = input.length() - 1; i >= 0; i -=1) {
+            reversed.append(input.charAt(i));
         }
-    
-        public static void main(String[] args) {
-            String original = "Hello, World!";
-            String reversed = reverseString(original);
-            System.out.println("Original: " + original);
-            System.out.println("Reversed: " + reversed);
-        }
+
+        // Convert the StringBuilder to a String and return it
+        return reversed.toString();
     }
+}
     
